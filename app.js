be40f78dad9ef7e5ea61ef4e8a68379400124f93
katty1398 <kate.r.c.13@hotmail.com>
2017-11-06 11:12:19 -0300

    twitter

---
+++ app.js
@@ -0,0 +1,25 @@
+
+function add(){
+
+  var comentario = document.getElementById('comentario').value;
+
+  document.getElementById('comentario').value = ' ';
+
+  var contenedor = documet.getElementById('contenedor');
+
+  var newComments = document.createElement('div');
+
+  var contenedorElemento = document.createElement('p');
+
+  }
+
+function cuenta(){
+    var longi= 140;
+    var res= "";
+    var obs= document.getElementById("obs").value.length;
+    res= longi - obs;
+
+
+}
+
+      
